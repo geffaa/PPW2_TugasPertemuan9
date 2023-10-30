9bc0a1a53b29ee49e4269a2d0764da3f26c3dedd
geffaa <yodhimas02@gmail.com>
2023-10-30 09:55:01 +0700

    edit views

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -10,15 +10,17 @@
 <body class="bg-gray-100">
 
 <x-app-layout>
-    <div class="container mx-auto mt-10 p-4">
-        <h1 class="text-3xl font-semibold text-center mb-6 bg-gray-200 py-2">Daftar Buku</h1>
-        <a href="{{ route('buku.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 inline-block">Tambah Buku</a>
-        <div class="mb-4">
-    <form action="{{ route('buku.search') }}" method="GET">
-        <input type="text" name="search" placeholder="Cari Buku" class="rounded-l py-2 px-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" />
-        <button type="submit" class="px-4 py-2 bg-blue-500 text-white font-bold rounded-r">Cari</button>
-    </form>
-</div>
+<div class="container mx-auto mt-10 p-4">
+        <h1 class="text-3xl font-semibold text-center mb-6 bg-gray-300 py-2">Daftar Buku</h1>
+        <div class="mt-4 mb-4 p-4 bg-white shadow-md flex items-center justify-between">
+            <a href="{{ route('buku.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah Buku</a>
+            <form action="{{ route('buku.search') }}" method="GET" class="flex items-center">
+                @csrf
+                <input type="text" name="kata" class="border rounded-l py-2 px-3 w-full" placeholder="Cari judul atau penulis...">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white rounded-r px-4 py-2">Cari</button>
+            </form>
+        </div>
+
 
         <table class="w-full border-collapse border border-gray-300 bg-white shadow-md">
             <thead class="bg-gray-200">
@@ -42,12 +44,12 @@
                     <td class="px-4 py-2 border border-gray-300">{{ $buku->penulis }}</td>
                     <td class="px-4 py-2 border border-gray-300">Rp {{ number_format($buku->harga, 2) }}</td>
                     <td class="px-4 py-2 border border-gray-300">{{ $buku->tgl_terbit }}</td>
-                    <td class="px-4 py-2 border border-gray-300">
-                        <a href="{{ route('buku.edit', $buku->id) }}" class="text-blue-500 hover:text-blue-700">Edit</a>
+                    <td class="px-4 py-2 border border-gray-300"> <!-- Menambahkan kelas "border" pada elemen ini -->
+                    <a href="{{ route('buku.edit', $buku->id) }}" class="border border-blue-500 text-blue-500 hover:text-blue-700 hover:border-blue-700 rounded px-3 py-1 mr-2">Edit</a>
                         <form action="{{ route('buku.destroy', $buku->id) }}" method="POST" class="inline">
-                            @csrf
-                            @method('DELETE')
-                            <button onclick="return confirm('Yakin mau dihapus?')" class="text-red-500 hover:text-red-700">Hapus</button>
+                        @csrf
+                        @method('DELETE')
+                        <button onclick="return confirm('Yakin mau dihapus?')" class="border border-red-500 text-red-500 hover:text-red-700 hover:border-red-700 rounded px-3 py-1">Hapus</button>
                         </form>
                     </td>
                 </tr>
